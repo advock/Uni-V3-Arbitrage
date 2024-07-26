a459f98e3b745026c8c1d817726f2a0980572c2b
advock <advaiyakarnik@gmail.com>
2024-07-26 23:49:53 +0530

    global backend added

---
+++ src/fork/simulator/fork_db.rs
@@ -0,0 +1,28 @@
+use std::sync::mpsc::channel;
+
+use futures::channel::mpsc::Sender;
+
+use revm::{
+    db::{CacheDB, DatabaseRef, EmptyDB},
+    primitives::{
+        Account, AccountInfo, Address as rAddress, Bytecode as rBytecode, HashMap as rHashMap,
+        B256, KECCAK_EMPTY, U256 as rU256,
+    },
+    Database, DatabaseCommit,
+};
+
+use super::{
+    data_base_errors::{DatabaseError, DatabaseResult},
+    BackendFetchRequest,
+};
+
+pub struct ForkDB {
+    backend: Sender<BackendFetchRequest>,
+    db: CacheDB<EmptyDB>,
+}
+
+impl ForkDB {
+    pub fn new(backend: Sender<BackendFetchRequest>, db: CacheDB<EmptyDB>) -> Self {
+        Self { backend, db }
+    }
+}
--- src/fork/simulator/mod.rs
+++ src/fork/simulator/mod.rs
@@ -1,1 +1,3 @@
 pub mod data_base_errors;
+pub mod fork_db;
+pub mod global_backend;
